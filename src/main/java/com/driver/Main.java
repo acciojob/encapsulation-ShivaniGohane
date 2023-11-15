0baaa0c6d9e3ad7f6b19eda34f900eecf224cee7
ShivaniGohane <shivanigohane07@gmail.com>
2023-11-15 16:07:48 +0530

    Changes Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+//        rw.name = "xyz";
+//        System.out.println(rw.name);
+        // will get error because mane is private
+        rw.setName("xyz");
+        String ans = rw.getName();
+        System.out.println(ans);
+    }
 }
